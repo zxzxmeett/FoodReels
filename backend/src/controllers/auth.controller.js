e0d1abce41577b3590596a61939bd6d8a9025d70
zxzxmeett <meet0201parmar@gmail.com>
2026-04-30 16:20:38 +0530

    Allows the cookie to be sent from Vercel to Render

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -31,7 +31,12 @@ async function registerUser(req, res) {
     process.env.JWT_SECRET
   );
 
-  res.cookie("token", token);
+  res.cookie("token", token, {
+    httpOnly: true,
+    secure: true, 
+    sameSite: "none",
+    maxAge: 24 * 60 * 60 * 1000 
+});
 
   res.status(201).json({
     message: "User registered successfully",
@@ -71,7 +76,12 @@ async function loginUser(req, res) {
     process.env.JWT_SECRET
   );
 
-  res.cookie("token", token);
+  res.cookie("token", token, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "none",
+    maxAge: 24 * 60 * 60 * 1000
+  });
 
   res.status(200).json({
     message: "User logged in successfully",
@@ -121,7 +131,12 @@ async function registerFoodPartner(req, res) {
     process.env.JWT_SECRET
   );
 
-  res.cookie("token", token);
+  res.cookie("token", token, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "none",
+    maxAge: 24 * 60 * 60 * 1000
+  });
 
   res.status(201).json({
     message: "Food partner registered successfully",
@@ -164,7 +179,12 @@ async function loginFoodPartner(req, res) {
     process.env.JWT_SECRET
   );
 
-  res.cookie("token", token);
+  res.cookie("token", token, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "none",
+    maxAge: 24 * 60 * 60 * 1000
+  });;
 
   res.status(200).json({
     message: "Food partner logged in successfully",
